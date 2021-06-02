25997411591c95850a60b5de09264f54d699b03d
Dieter Jackson <jacksondieter@gmail.com>
2021-06-02 11:10:51 +0200

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application } from 'express'
+
+const app: Application = express()
+const port = 3000
+
+app.use('/', (req, res) => {
+  res.status(200).send('working')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
