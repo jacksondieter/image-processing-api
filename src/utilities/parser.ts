3a2677cb9bb04c1d39154dba77d4f90431b4d146
Dieter Jackson <jacksondieter@gmail.com>
2021-06-17 00:39:38 +0200

    refactor: Clean unnecesary export

--- src/utilities/parser.ts
+++ src/utilities/parser.ts
@@ -1,6 +1,6 @@
 import { checkKey, TypeSpec } from 'query-validation'
 
-export const EXT_FORMAT: string[] = ['avif', 'jpeg', 'jpg', 'png', 'raw', 'tiff', 'webp']
+const EXT_FORMAT: string[] = ['avif', 'jpeg', 'jpg', 'png', 'raw', 'tiff', 'webp']
 // export const EXT_FORMAT_IN: string[] = ['avif','gif','jpeg','jpg','png','svg','tiff','webp']
 function parseFromString(str: string): string | number {
   if (!isNaN(+str)) {
@@ -10,7 +10,7 @@ function parseFromString(str: string): string | number {
   }
 }
 
-export function validate(body: Record<string, unknown>, keys: Record<string, TypeSpec>): { error?: string } {
+function validate(body: Record<string, unknown>, keys: Record<string, TypeSpec>): { error?: string } {
   for (const key in keys) {
     if (!checkKey(body[key], keys[key])) {
       return { error: `missing or invalid parameter ${key}` }
@@ -19,7 +19,7 @@ export function validate(body: Record<string, unknown>, keys: Record<string, Typ
   return {}
 }
 
-export function parse(body: Record<string, unknown>, keys: Record<string, TypeSpec>): Record<string, unknown> {
+function parse(body: Record<string, unknown>, keys: Record<string, TypeSpec>): Record<string, unknown> {
   const result: Record<string, unknown> = {}
   for (const key in keys) {
     result[key] = parseFromString(body[key] as string)
